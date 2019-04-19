edddd688c588fb14f66519b8104fb53145c169ab
Maarten Keet <m.keet@student.fontys.nl>
2019-04-19 15:29:24 +0200

    Display feedback correctly + implement addFeedback
    Implementation with external backend

--- src/app/submit-feedback/submit-feedback.component.ts
+++ src/app/submit-feedback/submit-feedback.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FeedbackService } from '../feedback.service';
 import { Feedback } from '../feedback';
 import { Response } from '../response';
+import { UserBase } from '../user-base';
 
 @Component({
   selector: 'app-submit-feedback',
@@ -10,19 +11,28 @@ import { Response } from '../response';
 })
 export class SubmitFeedbackComponent implements OnInit {
   response: Response;
+  feedbacks: Feedback[];
 
   constructor(private feedbackService: FeedbackService) { }
 
-  // addFeedback(author: string, recipient: string, content: string): void {
-  //   this.feedbackService.add({author, recipient, content} as Feedback)
-  //     .subscribe(feedback => {
-  //         this.feedbacks.push(feedback);
-  //       });
-  // }
+  addFeedback(author: string, recipient: string, content: string): void {
+    const feedbackGiver: UserBase = {
+      username: author
+    };
+    const feedbackTaker: UserBase = {
+      username: recipient
+    };
+    this.feedbackService.add({feedbackGiver, feedbackTaker, feedbackText: content} as Feedback)
+      .subscribe(response => {
+          this.response = response;
+          this.feedbacks.push(response.entity as Feedback);
+        });
+  }
 
   getFeedback(): void {
     this.feedbackService.get().subscribe(response => {
       this.response = response;
+      this.feedbacks = response.entity as Feedback[];
     });
   }
 
