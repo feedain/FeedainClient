1aefcc81ef8c34cafc47a1a9a829235573fc0de8
Maarten Keet <m.keet@student.fontys.nl>
2019-06-13 15:01:18 +0200

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { RouterModule, Routes } from "@angular/router";
 
 import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
 
@@ -9,6 +10,15 @@ import { AppComponent } from "./app.component";
 import { SubmitWorkComponent } from "./submit-work/submit-work.component";
 import { InMemoryDataService } from "./in-memory-data.service";
 import { CheckSubmissionComponent } from "./check-submission/check-submission.component";
+
+const appRoutes: Routes = [
+  { path: "submitWork", component: SubmitWorkComponent },
+  { path: "checkSubmission", component: CheckSubmissionComponent },
+  { path: "",
+    redirectTo: "/submitWork",
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   declarations: [
@@ -22,6 +32,9 @@ import { CheckSubmissionComponent } from "./check-submission/check-submission.co
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(
       InMemoryDataService, { dataEncapsulation: false }
+    ),
+    RouterModule.forRoot(
+      appRoutes
     )
   ],
   providers: [],
