797a3f9743b59d4be3dd94106048bf1d0bc642a0
Maarten Keet <m.keet@student.fontys.nl>
2019-06-14 11:29:08 +0200

    Add Give feedback content till work

--- src/app/give-feedback/give-feedback.component.ts
+++ src/app/give-feedback/give-feedback.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from "@angular/core";
 
+import { Work } from "../work";
+import { WorkService } from "../work.service";
+
 @Component({
   selector: "app-give-feedback",
   templateUrl: "./give-feedback.component.html",
   styleUrls: ["./give-feedback.component.css"]
 })
 export class GiveFeedbackComponent implements OnInit {
+  work: Work;
+  workId = 0;
 
-  constructor() { }
+  constructor(private workService: WorkService) { }
 
   ngOnInit() {
+    this.getWork();
   }
 
+  getWork() {
+    this.workService.getWork(this.workId).subscribe(work => this.work = work);
+    this.workId++;
+  }
 }
