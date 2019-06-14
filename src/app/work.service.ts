362d3a991ced1232aec3eb1c794904158ccb08ed
Maarten Keet <m.keet@student.fontys.nl>
2019-06-14 09:30:04 +0200

    Adjust authentication

--- src/app/work.service.ts
+++ src/app/work.service.ts
@@ -8,8 +8,8 @@ import { Work } from "./work";
 const httpOptions = {
   headers: new HttpHeaders({
     "Content-Type": "application/json",
-    Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NjA0NTMwNTMsInVzZXJuYW1lIjoiSmltIiwib3JnTmFtZSI6Ik9yZzEiLCJpYX"
-      + "QiOjE1NjA0MTcwNTN9.YWYGsmiG-ul0ciGoUgmtAlvqMu75Xxk7TAErE9LlUEk"
+    Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NjA1MzMyMzYsInVzZXJuYW1lIjoiSmltIiwib3JnTmFtZSI6Ik9yZzEiLCJpYX"
+    + "QiOjE1NjA0OTcyMzZ9.jBpJQ5NB9GeeqG7odAzWFe6LubYdswp7ugrWCPHh1mg"
   })
 };
 
@@ -18,13 +18,13 @@ const httpOptions = {
 })
 export class WorkService {
   // workUrl = "api/works";
-  workUrl = "http://145.93.92.181:4000/channels/mychannel/chaincodes/mycc";
+  workUrl = "http://145.93.92.252:4000/channels/mychannel/chaincodes/mycc";
 
   constructor(private http: HttpClient) { }
 
   getWork(): Observable<string> {
     // return this.http.get<string>(this.workUrl + "/0");
-    return this.http.get<string>(this.workUrl + "?peer=peer0.org1.example.com&fcn=query&args=[\"a\"]", httpOptions);
+    return this.http.get<string>(this.workUrl + "?peer=peer0.org1.example.com&fcn=query&args=[\"1\"]", httpOptions);
   }
 
   addWork(work: Work): Observable<Work> {
