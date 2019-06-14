d23848b6783eb1bb179442083ff3b38c5c6f3257
Maarten Keet <m.keet@student.fontys.nl>
2019-06-14 09:44:41 +0200

    Move work to backend

--- src/app/check-submission/check-submission.component.ts
+++ src/app/check-submission/check-submission.component.ts
@@ -3,6 +3,7 @@ import { FormControl } from "@angular/forms";
 
 import { Work } from "../work";
 import { Feedback } from "../feedback";
+import { WorkService } from "../work.service";
 
 @Component({
   selector: "app-check-submission",
@@ -11,24 +12,14 @@ import { Feedback } from "../feedback";
 })
 export class CheckSubmissionComponent implements OnInit {
   keyContent = new FormControl("");
-  work: Work = {
-    id: 0,
-    published: new Date(),
-    content: "Donec at urna a sem finibus lacinia. Duis mollis pharetra sollicitudin. Maecenas maximus urna id vestibulum varius. Nulla id"
-      + " ipsum feugiat, mattis ex vel, consequat arcu. Vivamus sit amet rhoncus sem. Nam non elit commodo, eleifend nisi id, finibus veli"
-      + "t. Praesent condimentum semper velit, ut viverra nisl rhoncus at. Suspendisse luctus augue id libero pharetra dapibus. Mauris imp"
-      + "erdiet semper urna eget dictum. Praesent vehicula nunc tortor, at mattis nulla tempor eget. Nulla blandit orci vitae nunc lacinia"
-      + ", eget mollis urna posuere. Duis congue mauris dui, sed fermentum lacus fermentum vitae. Nunc eu fringilla eros. Sed semper eros "
-      + "metus, porttitor facilisis quam laoreet sed. Praesent quis turpis id urna auctor laoreet. ",
-    feedback: [
-      {submitted: new Date(), content: "test"},
-      {submitted: new Date(), content: "That's shit"}
-    ]
-  };
+  work: Work;
 
-  constructor() { }
+  constructor(private workService: WorkService) { }
 
   ngOnInit() {
   }
 
+  getWork() {
+    this.workService.getWork().subscribe(work => this.work = work);
+  }
 }
